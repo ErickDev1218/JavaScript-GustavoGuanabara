8de4bfb538b0be88ad0548425ace40e22c302d41
Antonio Erick Freitas Ferreira <erickpdm879@gmail.com>
2022-02-10 18:07:37 -0300

    Estudos dia 10/02

---
+++ Aula 13/ambiente.js
@@ -0,0 +1,42 @@
+/* Teste lógico no final*/
+var comer = 1
+do{
+    console.log(`${comer}º pedaço.`)
+    comer++
+} while (comer <= 8)
+
+
+
+
+/* Teste lógico no inicio
+var comer = 1
+while (comer <= 8){
+    console.log(`${comer}º pedaço.`)
+    comer++
+}
+*/
+
+/*
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+*/
+
+/* Teste lógico no inicio:
+var c = 1
+while (c <= 6){
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+}
+*/
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
