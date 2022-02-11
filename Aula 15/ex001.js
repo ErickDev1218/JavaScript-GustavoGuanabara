2a67fddae42e7d4d6477441d59c97962d2d09295
Antonio Erick Freitas Ferreira <erickpdm879@gmail.com>
2022-02-11 17:09:17 -0300

    Estudos dia 11/02

---
+++ Aula 15/ex001.js
@@ -0,0 +1,7 @@
+let a = [6, 7, 4, 3, 1, 2]
+let pos = a.indexOf(1)
+    if(pos == -1){
+        console.log('O valor não foi encontrado!')
+    } else{
+        console.log(`O valor está na posição ${pos}`)
+    }
