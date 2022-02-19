400487d36bf3606fe1aa8680c1c538bce1c7aab5
Antonio Erick Freitas Ferreira <erickpdm879@gmail.com>
2022-02-18 22:33:12 -0300

    Estudos dia 18/02

--- Ex003/script.js
+++ Ex003/script.js
@@ -9,18 +9,31 @@ function contar(){
 
     //Inicio da lógica
     if(beg.value.length == 0 || end.value.length == 0 || step.value.length == 0){
-        alert('[ERROR] Faltam valores.')
+        tell.innerHTML = 'Impossivel contar!'
+
     }else{
-        tell.innerHTML = 'Contando: '
+        tell.innerHTML = 'Contando: <br> '
         let b = Number(beg.value)
         let e = Number(end.value)
         let s = Number(step.value)
 
-        for(let count = b; count <= e; count += s){
-            tell.innerHTML += ` ${count} \u{1F449}`
+        if(s <=0){
+            alert('Consideraremos passo igual a 1')
+            s = 1
         }
+        if(b < e){//contagem crescente
+            for(let count = b; count <= e; count += s){
+                tell.innerHTML += ` ${count} \u{1F449}`
+            }
+        }else{//contagem decrescente
+            for(let Count = b; Count >= e; Count-= s){
+                tell.innerHTML += `${Count} \u{1F449}`
+            }
+        }
+
+        
         tell.innerHTML += `\u{1F3C1}`
-    }
+    } 
     }
 
 
